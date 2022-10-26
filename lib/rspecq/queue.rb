88637de0032869d34be6bfa55fc51eb3b3023bb2
Stefanos Boglou <stefb@skroutz.gr>
2022-10-26 15:33:52 +0300

    Correctly allow re-runs with the reproduction flag
    There is no reason to have to randomize the build_id on each
    reproduction run, thus we can just override the rspecq state
    when we publish the queue.

--- lib/rspecq/queue.rb
+++ lib/rspecq/queue.rb
@@ -83,8 +83,19 @@ module RSpecQ
     end
 
     # NOTE: jobs will be processed from head to tail (lpop)
+    # Also some state keys are wiped to facilitate re-runs
+    # with the same job build prefix.
     def publish(jobs, fail_fast = 0)
+      cleanup_keys = [
+        key_queue_unprocessed, key_queue_running, key_queue_processed, key_failures,
+        key_flaky_failures, key_errors, key_requeues, key_example_count,
+        key_worker_heartbeats
+      ]
+
       redis.multi do |pipeline|
+        cleanup_keys.each do |key|
+          pipeline.del(key)
+        end
         pipeline.hset(key_queue_config, "fail_fast", fail_fast)
         pipeline.rpush(key_queue_unprocessed, jobs)
         pipeline.set(key_queue_status, STATUS_READY)
