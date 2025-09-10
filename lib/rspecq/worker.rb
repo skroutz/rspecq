81994d9961ab709ca03fb671ccdd3b2d9a104667
Christos Trochalakis <yatiohi@ideopolis.gr>
2025-09-10 12:10:28 +0300

    switch to sentry-raven

--- lib/rspecq/worker.rb
+++ lib/rspecq/worker.rb
@@ -343,7 +343,7 @@ module RSpecQ
     def log_event(msg, level, additional = {})
       warn msg
 
-      Sentry.capture_message(msg, level: level, extra: {
+      Raven.capture_message(msg, level: level, extra: {
         build: @build_id,
         worker: @worker_id,
         queue: queue.inspect,
