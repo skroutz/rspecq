647be008ea71773ee8f19e51aa68f55591b4f829
Christos Trochalakis <yatiohi@ideopolis.gr>
2025-09-18 11:47:55 +0300

    test: make start_worker() have the same signature as exec_build()

--- test/test_queue.rb
+++ test/test_queue.rb
@@ -4,9 +4,8 @@ class TestQueue < RSpecQTest
   def test_flaky_jobs
     build_id = rand_id
 
-    Process.wait(start_worker(build_id: build_id, suite: "flaky_job_detection"))
-
-    queue = RSpecQ::Queue.new(build_id, "foo", REDIS_OPTS)
+    pid, queue = start_worker("flaky_job_detection", build_id: build_id)
+    Process.wait(pid)
 
     assert_queue_well_formed(queue)
     refute queue.build_successful?
